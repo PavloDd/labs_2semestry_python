2b9232227637f4ecb0b98272c1849102a0231cf0
PavloDd <86347147+PavloDd@users.noreply.github.com>
2023-05-14 14:58:49 +0300

    added private poles

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ class Fridge:
 
     def __init__(self, brand="Bosch", model="CC11", capacity_in_liters=15, is_defrosing=False,
                  energy_efficiency_class='A'):
-        self._brand = brand
-        self._model = model
-        self._capacity_in_liters = capacity_in_liters
-        self._is_defrosing = is_defrosing
-        self._energy_efficiency_class = energy_efficiency_class
+        self.__brand = brand
+        self.__model = model
+        self.__capacity_in_liters = capacity_in_liters
+        self.__is_defrosing = is_defrosing
+        self.__energy_efficiency_class = energy_efficiency_class
 
     def turn_on_defrosing(self):
-        self._is_defrosing = True
+        self.__is_defrosing = True
 
     def turn_off_defrosing(self):
-        self._is_defrosing = False
+        self.__is_defrosing = False
 
     def delete_model_info(self):
-        del self._model
+        del self.__model
 
     @staticmethod
     def get_instance():
@@ -25,8 +25,8 @@ class Fridge:
         return Fridge.instance
 
     def __str__(self):
-        return f"Fridge(brand={self._brand}, model={self._model}, capacity_in_liters={self._capacity_in_liters}," \
-               f" is_defrosing={self._is_defrosing}, energy_efficiency_class={self._energy_efficiency_class})"
+        return f"Fridge(brand={self.__brand}, model={self.__model}, capacity_in_liters={self.__capacity_in_liters}," \
+               f" is_defrosing={self.__is_defrosing}, energy_efficiency_class={self.__energy_efficiency_class})"
 
 
 fridges = [Fridge("Samsung", "SS13", 15, False, 'A'), Fridge(), Fridge.get_instance(), Fridge.get_instance()]
